fcea7f920e8224603747c8ada38799f00ed42b77
tinymeng <666@majiamengcom>
2022-07-08 21:53:50 +0800

    update

--- Application/Home/Controller/CallbackController.class.php
+++ Application/Home/Controller/CallbackController.class.php
@@ -3,18 +3,38 @@ namespace Home\Controller;
 
 class CallbackController extends PublicController {
 
+    /**
+     * 支付回调
+     * @return void
+     */
     public function pay(){
+        $result = [
+            'code' => 200,
+            'msg' => ''
+        ];
         $user_id = I('get.user_id',0,'intval');
-        var_dump($user_id);
-
-        $info = D('Resume')->where(array('user_id'=>$user_id))->find();
-        var_dump($info);
-        if(!empty($info)) {
-
+        if(empty($user_id)){
+            $result['code'] = 400;
+            $result['msg'] = '参数缺失';
+            $this->jsonSuccess($result);
         }
 
-        var_dump(111);
-        die;
+        $where = array('user_id'=>$user_id);
+        $info = D('Resume')->where($where)->find();
+        if(empty($info)) {
+            $result['code'] = 400;
+            $result['msg'] = '数据不存在';
+            $this->jsonSuccess($result);
+        }
+        $data = [
+            'is_pay' =>1,
+        ];
+        D('Resume')->where($where)->save($data);
+        $this->jsonSuccess($result);
+    }
+
+    public function jsonSuccess($result){
+        echo json_encode($result);exit();
     }
    
 }
