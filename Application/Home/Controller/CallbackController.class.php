c0f25c4613fbdb26daeb1c074213101de72258cf
tinymeng <666@majiamengcom>
2022-07-08 21:48:41 +0800

    update

--- Application/Home/Controller/CallbackController.class.php
+++ Application/Home/Controller/CallbackController.class.php
@@ -4,7 +4,17 @@ namespace Home\Controller;
 class CallbackController extends PublicController {
 
     public function pay(){
-        var_dump(111);die;
+        $user_id = I('get.user_id',0,'intval');
+        var_dump($user_id);
+
+        $info = D('Resume')->where(array('user_id'=>$user_id))->find();
+        var_dump($info);
+        if(!empty($info)) {
+
+        }
+
+        var_dump(111);
+        die;
     }
    
 }
