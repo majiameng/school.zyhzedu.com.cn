201f9bb19a0697933a6eeb816d179c70a9c3ff74
tinymeng <666@majiameng.com>
2022-07-11 09:49:33 +0800

    hm.src = "/js/an.js";

---
+++ Public/js/an.js
@@ -0,0 +1,23 @@
+//baidu 缁熻
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?c0b558a20071806ed9b0e5c2eb021f7e";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+// google analytics
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://www.googletagmanager.com/gtag/js?id=G-GPVEW585VG";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-GPVEW585VG');
+})();
